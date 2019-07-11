75f585a26ab0cac205721700044cf2527341ade6
Vaibhav Rathore <vaibhav@coloredcow.in>
2019-07-11 13:14:52 +0530

    refresh token on any input/select change

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function(){
 	jQuery('[name="api_connector_grant_type"]').on('change', function(){
 		jQuery('.credentials-block').addClass('d-none');
 		jQuery('#' + jQuery(this).val()).removeClass('d-none');
+	});
+	jQuery('input, select').on('change', function(){
 		jQuery('[name="api_connector_refresh_token"]').val('1');
 	});
 });
